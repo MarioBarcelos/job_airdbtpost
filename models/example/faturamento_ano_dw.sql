c1f74b6c49c063aec1c754ff4b5186168e74f5f9
MarioBarcelos <mariosbpimentel@gmail.com>
2023-08-07 23:00:15 -0300

    first

---
+++ models/example/faturamento_ano_dw.sql
@@ -0,0 +1,12 @@
+with faturamento_ano_dw as (
+
+    SELECT extract ("year" from purchased_at ) AS "Ano",
+            sum(ap.price) AS "Faturamento"
+    FROM public.airbyte_purchases pur INNER JOIN airbyte_products ap
+    ON pur.product_id = ap.id
+    GROUP BY 1 
+    ORDER BY 1 desc ;
+
+)
+
+SELECT * FROM faturamento_ano_dw
