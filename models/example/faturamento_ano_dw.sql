9b325601b4928456563f6ccfd0143d3cbedc5d81
MarioBarcelos <mariosbpimentel@gmail.com>
2023-08-10 08:11:37 -0300

    alter

--- models/example/faturamento_ano_dw.sql
+++ models/example/faturamento_ano_dw.sql
@@ -5,7 +5,7 @@ with faturamento_ano_dw as (
     FROM public.airbyte_purchases pur INNER JOIN airbyte_products ap
     ON pur.product_id = ap.id
     GROUP BY 1 
-    ORDER BY 1 desc ;
+    ORDER BY 1 desc
 
 )
 
